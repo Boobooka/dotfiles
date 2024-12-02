e3799f71f72b69ebb3dade53db4d14e2489ad71e
Méhész György Ákos <boobooka@gmail.com>
2024-12-02 02:49:45 +0100

    top bar ikonok stb:wq

--- spectrwm/.config/spectrwm/baraction.sh
+++ spectrwm/.config/spectrwm/baraction.sh
@@ -1,11 +1,17 @@
 #!/bin/zsh
 
-SLEEP_TIME=2
+SLEEP_TIME=1
 
 vol() {
-  vol=`amixer get Master | awk -F "[][]" 'END{ print $4,$2}'`
-  #echo -e "\U1F50A $vol"
-  echo -e "+@fg=3;Vol: $vol+@fg=0;"
+  amixer get Master | awk -F "[][]" 'END{ print $2,$4}' | read vol state
+  mic=`amixer get Capture | awk -F "[][]" 'END{print $4}'`
+  icon="󰕾"
+  [[ $mic = off ]] && micoff=" +@fg=2;󰍭+@fg=0;"
+  fg=3
+  [[ $vol = "0%" ]] && icon="󰝟+"
+  [[ $state = off ]] &&  icon="󰖁" && fg=2
+   
+  echo -e "+@fg=$fg;$icon+@fg=0; $vol$micoff"
 }
 
 cpu() {
@@ -18,27 +24,29 @@ cpu() {
   idlediff=$((idle-previdle))
   cpu=$(( 100 * (totaldiff - idlediff) / totaldiff ))
  
-  echo -e "Cpu: $cpu%"
+  echo -e "+@fg=3;+@fg=0; $cpu%"
 }
 
 mem() {
   free | awk '/Mem/ {print $2,$3}' | read total used
   p=0; (( p += (used / (total / 100)) ))
+  pre=""
+  me="MiB"
   if ((p > 90)) {
-    pre="+@fg=2;"; post="+@fg=0;"
-  } else { 
-    pre=""; post=""
+    pre="+@fg=2;" 
   }
+  post="+@fg=0;"
   (( used /= 1024))
-  echo -e "Mem: $used MiB ($pre$p%$post)"
+  echo -e "+@fg=3;$post $used$me ($pre$p%$post)"
 }
 
 hdd() {
-  df -h | awk '/\/home/ {print $4,$5}' | read free perc
-  echo -e "Hdd: $free ($perc)"
+  df -h | awk '/\/$/ {print $4,$5}' | read r_free r_perc
+  df -h | awk '/\/home$/ {print $4,$5}' | read h_free h_perc
+  echo -e "+@fg=3;󱂵+@fg=0; $h_free ($h_perc) +@fg=3;+@fg=0; $r_free ($r_perc)"
 }	
 
 while :; do
-	echo -e "$(hdd)|$(cpu)|$(mem)|$(vol)"
+	echo -e "$(hdd) $(cpu) $(mem) $(vol)"
   sleep $SLEEP_TIME
-done	
+done
