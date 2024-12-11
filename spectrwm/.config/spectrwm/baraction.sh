994645176f3ae206cef7bef8ca408831b6d48101
Méhész György Ákos <boobooka@gmail.com>
2024-12-11 02:59:58 +0100

    Spectrwm config and script cleanup

--- spectrwm/.config/spectrwm/baraction.sh
+++ spectrwm/.config/spectrwm/baraction.sh
@@ -1,20 +1,49 @@
 #!/bin/zsh
 
-SLEEP_TIME=2
+################################
+###          Colors          ###
+################################
+
+# should get 2 parameters text and color
+color() {
+  local colors=(active alert icon bt) # Anything else will get the default color (0)
+  echo -e "+@fg=${colors[(I)$2]};$1+@fg=0;"  
+}
+
+################################
+###          Icons           ###
+################################
+
+# getIcon
+gI() {
+  typeset -A icons
+  local icons=(
+    "monitor"   "\Uf0379" # 󰍹
+    "home"      "\Uf10b5" # 󱂵
+    "root"      "\Uf17c"  # 
+    "cpu"       "\Uf4bc"  # 
+    "mem"       "\Ue266"  # 
+    "vol"       "\Uf057e" # 󰕾
+    "vol0"      "\Uf057f" # 󰕿
+    "vol-mute"  "\Uf0581" # 󰖁
+    "mic-mute"  "\Uf036d" # 󰍭
+    "bt"        "\Uf0970" # 󰥰 bluetooth headset
+  )
+  echo -e "$(color $icons[$1] ${2:=icon})"
+}
+
 
 vol() {
   amixer get Master | awk -F "[][]" 'END{ print $2,$4}' | read vol state
   mic=`amixer get Capture | awk -F "[][]" 'END{print $4}'`
-  icon="󰕾"
-  [[ $mic = off ]] && micoff=" +@fg=2;󰍭+@fg=0;"
-  fg=3
-  [[ $vol = "0%" ]] && icon="󰝟" && fg=0
-  [[ $state = off ]] &&  icon="󰖁" && fg=2
-  
   batt=`bluetoothctl info 00:09:B0:2C:B6:B8 | awk '{gsub (/[)(]/, "")} END{print $4}'`
-  [[ -n $batt ]] && bt=" +@fg=4;󰥰+@fg=0; $batt%"    
-   
-  echo -e "+@fg=$fg;$icon+@fg=0; $vol$micoff$bt"
+
+  [[ $vol = "0%" ]] && icon=$(gI vol0 def) || icon=$(gI vol)
+  [[ $state = off ]] && icon=$(gI vol-mute alert)
+  [[ $mic = off ]] && micoff=" $(gI mic-mute alert)"
+  [[ -n $batt ]] && bt=" $(gI bt bt) $batt%"
+
+  echo -e "$icon $vol$micoff$bt"
 }
 
 cpu() {
@@ -27,30 +56,31 @@ cpu() {
   idlediff=$((idle-previdle))
   cpu=$(( 100 * (totaldiff - idlediff) / totaldiff ))
  
-  echo -e "+@fg=3;+@fg=0; $cpu%"
+  echo -e "$(gI cpu) $cpu%"
 }
 
 mem() {
   free | awk '/Mem/ {print $2,$3}' | read total used
   p=0; (( p += (used / (total / 100)) ))
-  pre=""
-  me="MiB"
-  if ((p > 90)) {
-    pre="+@fg=2;" 
-  }
-  post="+@fg=0;"
+  (( p > 90 )) && p=$(color "$p%" alert) || p="$p%"
   (( used /= 1024))
-  echo -e "+@fg=3;$post $used $me ($pre$p%$post)"
+  
+  echo -e "$(gI mem) $used MiB ($p)"
 }
 
 hdd() {
   DF=`df -h`
   echo $DF | awk '/\/$/ {print $4,$5}' | read r_free r_perc
   echo $DF | awk '/\/home$/ {print $4,$5}' | read h_free h_perc
-  echo -e "+@fg=3;󱂵+@fg=0; $h_free ($h_perc) +@fg=3;+@fg=0; $r_free ($r_perc)"
-}	
+
+  echo -e "$(gI home) $h_free ($h_perc) $(gI root) $r_free ($r_perc)"
+}
+
+clk() {
+  date "+%Y %b %d.%a %R"
+}
 
 while :; do
-  echo -e "$(hdd) $(cpu) $(mem) $(vol)"
-  sleep $SLEEP_TIME
+  echo -e "+|L$(gI monitor) +N:+I <+D> +S +L+|T(+w|+M) +|C <+W> +|R $(hdd) $(cpu) $(mem) $(vol) +|T$(clk)"
+  sleep 1
 done
